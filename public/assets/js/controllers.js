b76bdd5a7562eaa9f6a8fb6b855ac8868b7d6e5b
Bruno Barbosa <bruno@barbosa.io>
2016-04-30 10:40:58 -0700

    Filter refactoring

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -57,28 +57,14 @@ app.controller('pokemonCtrl', function($scope, $state, Pokemon) {
 })
 
 app.filter('capitalize', function() {
-  return function(input){
-    if(input.indexOf(' ') !== -1){
-      var inputPieces,
-          i;
-
-      input = input.toLowerCase();
-      inputPieces = input.split(' ');
-
-      for(i = 0; i < inputPieces.length; i++){
-        inputPieces[i] = capitalizeString(inputPieces[i]);
-      }
-
-      return inputPieces.toString().replace(/,/g, ' ');
-    }
-    else {
-      input = input.toLowerCase();
-      return capitalizeString(input);
+  return function(str){
+    if(!str) {
+      return str;
     }
 
-    function capitalizeString(inputString){
-      return inputString.substring(0,1).toUpperCase() + inputString.substring(1);
-    }
+    return str.split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase() )
+      .join(' ')
   };
 });
 
